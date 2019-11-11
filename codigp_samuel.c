8d0dd7667697bf5c99720e39552f78dcd4121e80
WagnerLancetti <56642195+WagnerLancetti@users.noreply.github.com>
2019-11-11 15:14:22 -0300

    codigo de teste

---
+++ codigp_samuel.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int gera_valor(int x, int valor){
+    srand(time(NULL));
+    x = (rand() % valor) + 1;
+    return x;
+}
+
+
+int main(){
+    int i, valor = ;
+    FILE *arq = fopen("input.txt","w");
+    int vert;
+    scanf("%i" , &vert);
+    for(i = 0; i != valor;i++){
+
+    }
+}
